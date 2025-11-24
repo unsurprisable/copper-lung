41924a87fa2fc7b2f9c5431d8060b0455d5181f8
Brady Silvers <toastedflqme@gmail.com>
2025-11-23 18:09:29 -0800

    Project setup

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("net.minestom:minestom:2025.10.31-1.21.10")
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Main {
+    static void main() {
+        System.out.println("Hello World!");
+    }
+}
